04796a7292949dae02b5657dddb9da61911eb2bf
Carlos Eduardo Bahia <cadu29bahia@gmail.com>
2024-05-19 20:59:56 -0300

    Limpando a main

--- main.cpp
+++ main.cpp
@@ -9,59 +9,9 @@ using namespace std;
 
 int main() {
 
+    //Parametros: Kmeans(Base de Dados, Numero de Clusters)
+    //1 - Iris, 2 - MFeat
     kmeans(2,10);
 
-    /*
-    Testes:
-
-
-    auto start = chrono::high_resolution_clock::now();
-
-    vector<Instancia> instanciasIris = Instancia::lerIris();
-    vector<Instancia> instanciasMFeat = Instancia::lerMFeat();
-    vector<Centroide> centroidesIris = criarCentroidesAleatorios(3,instanciasIris);
-    vector<Centroide> centroidesMfeat = criarCentroidesAleatorios(10, instanciasMFeat);
-
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end-start);
-
-    start = chrono::high_resolution_clock::now();
-
-    calcularCentroidesProximos(centroidesIris, instanciasIris);
-    calcularCentroidesProximos(centroidesMfeat, instanciasMFeat);
-
-    end = chrono::high_resolution_clock::now();
-
-    auto durationCalculoInicial = chrono::duration_cast<chrono::milliseconds>(end-start);
-
-    start = chrono::high_resolution_clock::now();
-
-    atualizarCentroides(centroidesIris);
-    atualizarCentroides(centroidesMfeat);
-
-    end = chrono::high_resolution_clock::now();
-
-    auto durationAtualizacao = chrono::duration_cast<chrono::milliseconds>(end-start);
-
-    start = chrono::high_resolution_clock::now();
-
-    Instancia::escreverInstancias(instanciasIris, "instancias_iris.txt");
-    Instancia::escreverInstancias(instanciasMFeat, "instancias_mfeat.txt");
-    Centroide::escreverCentroide(centroidesIris, "centroides_iris.txt");
-    Centroide::escreverCentroide(centroidesMfeat, "centroides_mfeat.txt");
-    Centroide::escreverCentroidesComInstancias(centroidesIris, "centroides_iris.txt");
-    Centroide::escreverCentroidesComInstancias(centroidesMfeat, "centroides_mfeat.txt");
-
-    end = chrono::high_resolution_clock::now();
-    auto durationEscrita = chrono::duration_cast<chrono::milliseconds>(end-start);
-
-    cout << "K-Means Finalizado." << endl;
-    cout << "Informações importantes:" << endl;
-    cout << "Tempo total de Instanciação: " << duration.count() << " milissegundos." << endl;
-    cout << "Tempo total de calculo de Instancias Proximas: " << durationCalculoInicial.count() << " milissegundos." << endl;
-    cout << "Tempo total de Atualização dos centroides: " << durationAtualizacao.count() << " milissegundos." << endl;
-    cout << "Tempo total de escrita de arquivos: " << durationEscrita.count() << " milissegundos." << endl;
-    */
-    
     return 0;
 }
